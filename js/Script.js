cb087efddf6cc1dffb65a92eec166905f34fe543
ojinikim <ojinikim@gmail.com>
2023-10-25 14:45:18 +0900

    nn

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,11 @@
+<<<<<<< Updated upstream
+=======
+// const btns = document.querySelectorAll('button');
+// btns.forEach((element) => {
+//   element.onclick = () => console.log("CLicked");
+// });
+
+>>>>>>> Stashed changes
 const modalOpenButton = document.getElementById("account-menu-icon");
 const modal = document.getElementById("account-modal");
 
@@ -7,4 +15,8 @@ modalOpenButton.addEventListener("click", () => {
   } else {
     modal.classList.add("hidden");
   }
+<<<<<<< Updated upstream
 }
+=======
+});
+>>>>>>> Stashed changes

2cd64a2a01ad81bef5318506e287eefd325cc2fb
hwibaek <hwibaek2827@gmail.com>
2023-10-25 14:01:12 +0900

    Js 복원

--- js/Script.js
+++ js/Script.js
@@ -1,4 +1,10 @@
-const btns = document.querySelectorAll('button');
-btns.forEach((element) => {
-  element.onclick = () => console.log("CLicked");
-});
+const modalOpenButton = document.getElementById("account-menu-icon");
+const modal = document.getElementById("account-modal");
+
+modalOpenButton.addEventListener("click", () => {
+  if (modal.classList.value.includes("hidden")) {
+    modal.classList.remove("hidden");
+  } else {
+    modal.classList.add("hidden");
+  }
+}

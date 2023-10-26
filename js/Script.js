03bd37419bf8d62f16f8830344c21d39ae958272
hwibaek <hwibaek2827@gmail.com>
2023-10-26 11:58:43 +0900

    Js 이상한거 지움

--- js/Script.js
+++ js/Script.js
@@ -1,11 +1,4 @@
-<<<<<<< Updated upstream
-=======
-// const btns = document.querySelectorAll('button');
-// btns.forEach((element) => {
-//   element.onclick = () => console.log("CLicked");
-// });
 
->>>>>>> Stashed changes
 const modalOpenButton = document.getElementById("account-menu-icon");
 const modal = document.getElementById("account-modal");
 
@@ -15,8 +8,4 @@ modalOpenButton.addEventListener("click", () => {
   } else {
     modal.classList.add("hidden");
   }
-<<<<<<< Updated upstream
-}
-=======
 });
->>>>>>> Stashed changes
